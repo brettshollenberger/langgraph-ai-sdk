cd6d8d9c9a25225f255e79fa634525ec06a9d854
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-11-04 12:48:14 -0500

    Add node middleware tests

--- packages/langgraph-ai-sdk/src/testing/node/nodeMiddleware.ts
+++ packages/langgraph-ai-sdk/src/testing/node/nodeMiddleware.ts
@@ -3,9 +3,7 @@ import { withErrorHandling } from "./withErrorHandling";
 import { withNotifications } from "./withNotifications";
 import { NodeMiddlewareFactory } from "./nodeMiddlewareFactory";
 
-const _nodeMiddleware = new NodeMiddlewareFactory()
+export const NodeMiddleware = new NodeMiddlewareFactory()
   .addMiddleware("context", withContext)
   .addMiddleware("notifications", withNotifications)
   .addMiddleware("errorHandling", withErrorHandling);
-
-export const NodeMiddleware = _nodeMiddleware;
